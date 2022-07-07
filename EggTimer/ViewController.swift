b5c061a37df711612104fc169235fe2564462814
horamupa <horamupa@gmail.com>
2022-07-07 17:40:06 +0200

    switch

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -8,7 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let softTime =  5
+    let mediumTime = 6
+    let hardTime = 9
+    
 
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        let hardness = sender.titleLabel?.text
+        switch hardness {
+        case "4": print("Soft")
+        case "5": print("Soft")
+        case "6": print("Medium")
+        case "7": print("Medium")
+        case "8": print("Hard")
+        case "9": print("Hard")
+        default: print("Error")
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
