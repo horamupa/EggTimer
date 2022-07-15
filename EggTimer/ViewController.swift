57d4e1173e2b9a3f0bb4ac52b907695dc3d3b8aa
horamupa <horamupa@gmail.com>
2022-07-15 16:14:01 +0200

    go go

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -9,25 +9,41 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let softTime =  5
-    let mediumTime = 6
-    let hardTime = 9
+    let boilTime = ["4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9,]
+    var timer = Timer()
+    var count: Float = 0
     
-
+    @IBOutlet var mainLabel: UILabel!
+    
+    @IBOutlet var progressBar: UIProgressView!
+    
     @IBAction func buttonPressed(_ sender: UIButton) {
+        timer.invalidate()
+        
+        var secondsRemaining = Float(sender.titleLabel!.text!)!
+        
+//        var progressGo = Int(sender.titleLabel!.text!)!
+        
         let hardness = sender.titleLabel?.text
-        switch hardness {
-        case "4": print("Soft")
-        case "5": print("Soft")
-        case "6": print("Medium")
-        case "7": print("Medium")
-        case "8": print("Hard")
-        case "9": print("Hard")
-        default: print("Error")
-        }
+        
+        print(boilTime[hardness!]!)
+        timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { (Timer) in
+            
+            if secondsRemaining > self.count  {
+                    print ("\(secondsRemaining) seconds")
+                    self.progressBar.progress = Float(secondsRemaining) / Float(sender.titleLabel!.text!)!
+                self.count += 1
+                } else {
+                    self.mainLabel.text = "Done"
+                    Timer.invalidate()
+                }
+            }
+        
+        
     }
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view.
     }
 
